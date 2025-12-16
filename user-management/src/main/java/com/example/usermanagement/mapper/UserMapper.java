4118e04388849946a4458400b243031237133b73
mqyfeifei <3024819981@qq.com>
2025-12-16 14:53:59 +0800

    Delete user-management directory

--- user-management/src/main/java/com/example/usermanagement/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.usermanagement.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.usermanagement.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends BaseMapper<User> {
-}
